247eb7d93dc0beef10d531f2a2b5513dec2bf345
hroque01 <hadson.roque01@gmail.com>
2022-11-14 18:09:57 +0100

    Ciclo While: Lista Array nel DOM

--- main.js
+++ main.js
@@ -1,18 +1,32 @@
-const listaSpesa = [ "cipolla" , "latte", "carne", "nutella"];
+const listaSpesa = [ "cipolla" , "latte", "carne", "nutella",];
 
 const miaLista = document.querySelector('.miaLista');
 
-console.log(miaLista)
+// console.log(miaLista)
 
-for ( let i = 0; i < listaSpesa.length; i++ ) {
+// for ( let i = 0; i < listaSpesa.length; i++ ) {
+
+//     let elementi = listaSpesa[i];
+
+//     // creo l'elemento li
+//     let liElement = document.createElement("li");
+//     liElement.innerHTML = elementi;
+
+//     //aggiungiamo i li all'ul
+//     miaLista.append(liElement);
+
+// }
+
+let i = 0
+while ( i < listaSpesa.length ) {
 
     let elementi = listaSpesa[i];
 
-    // creo l'elemento li
     let liElement = document.createElement("li");
     liElement.innerHTML = elementi;
 
-    //aggiungiamo i li all'ul
     miaLista.append(liElement);
+
+    i++;
 
 }
